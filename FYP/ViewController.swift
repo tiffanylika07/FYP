1c946bc0011ba3f235a492b162e64b583344dcd1
Tiffany Li <tiffanyli@Tiffany-Lis-Macbook-Pro.local>
2018-01-03 22:29:40 +0800

    Initial Commit

---
+++ FYP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FYP
+//
+//  Created by Tiffany Li on 3/1/2018.
+//  Copyright © 2018 Tiffany Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
